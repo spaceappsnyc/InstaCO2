1713c522fee4c5ec5a05674e7120880179587905
Sam Wellander <samwellander@gmail.com>
2018-10-21 12:17:34 -0400

    final

--- src/Callback.js
+++ src/Callback.js
@@ -32,19 +32,26 @@ export default class Callback extends Component {
     return total / images.length
   }
 
-  render() {
-    const score = this.calculateScore(this.state.images);
+  getLevel(score) {
     let level = '';
     if (score < 1) level = 'level_4'
     else if (score < 5) level = 'level_3'
     else if (score < 7) level = 'level_2'
     else if (score < 10) level = 'level_1'
     else if (score < 15) level = 'level_0'
+    $('#level').addClass(level);
+  }
+
+  render() {
+    const score = this.calculateScore(this.state.images);
+    setTimeout(() => {
+      this.getLevel(score)
+    }, 2000)
 
     return (
       <div class="callback_page box">
         {/* <h1>Your Score: {this.calculateScore(this.state.images)}</h1> */}
-        <div id="level" class={`box ${level}`}></div>
+        <div id="level" class=""></div>
       </div>
     );
   }
