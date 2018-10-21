de15b033f7a416534100fb3e47842d190d2c3c6e
Sam Wellander <samwellander@gmail.com>
2018-10-21 13:48:51 -0400

    remove env

--- .env.js
+++
@@ -1,6 +0,0 @@
-module.exports = {
-  INSTAGRAM_CLIENT_ID: '080eb63008dd41d0bcd80a1d6208d372',
-  INSTAGRAM_CLIENT_SECRET: '1e770397bfce4aafbc8f5ef0563066b8',
-  INSTAGRAM_REDIRECT_URI: 'http://localhost:3000/api/auth/instagram/callback',
-  CLARIFAI_KEY: 'd3fc32cb4fbd4d20a8dc057208d6ce4f'
-};
